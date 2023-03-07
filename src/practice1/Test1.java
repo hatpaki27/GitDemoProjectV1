7a6ace6b4843143b01b7be602b62fae4388bd483
Ravi <Ravi@Ravindra>
2023-03-07 11:13:04 +0530

    First commit....

---
+++ src/practice1/Test1.java
@@ -0,0 +1,21 @@
+package practice1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=1234;
+		int rev=0;
+		
+		while(num!=0)
+		{
+			rev=rev*10+num%10;
+			num=num/10;
+		}
+		
+		System.out.println(rev);
+
+	}
+
+}
